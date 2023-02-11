2f252ea04589e235e9fde5a977505c1d37b860cc
ValentinDealbera <valentindealbera01@gmail.com>
2023-02-10 22:05:02 -0300

    search on enter

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,32 +1,51 @@
 import React from "react";
-import styles from './SearchBar.module.css'
+import styles from "./SearchBar.module.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SearchBar(props) {
+  const [character, setCharacter] = useState("");
+  console.log(character);
+  const input = (evento) => {
+    setCharacter(evento.target.value);
+  };
 
-   const [character, setCharacter] = useState('')
-   console.log(character);
-   const input = (evento) => {
-      setCharacter(evento.target.value)
+  const handleKeyPress = (event) => {
+   if(event.key === 'Enter'){
+     props.onSearch(character)
+     setCharacter('')
    }
+ }
 
-   return (
-      <div className={styles.divGeneral}>
-         <div className={styles.buttonDiv}>
-            <Link to={'/about'}>
-               <span className={styles.button}>About</span>
-            </Link>
-            <Link to={'/home'}>
-               <span className={styles.button}>Home</span>
-            </Link>
-         </div>
-         <div className={styles.searchBar}>
-            <input placeholder="Search character ID" value={character} onChange={input} className={styles.input} type='search' />
-            <button className={styles.button} onClick={() => {
-               props.onSearch(character)
-               setCharacter('')}}>Agregar</button>
-         </div>
+  return (
+    <div className={styles.divGeneral}>
+      <div className={styles.buttonDiv}>
+        <Link to={"/about"}>
+          <span className={styles.button}>About</span>
+        </Link>
+        <Link to={"/home"}>
+          <span className={styles.button}>Home</span>
+        </Link>
       </div>
-   );
+      <div className={styles.searchBar}>
+        <input
+          placeholder="Search character ID"
+          value={character}
+          onChange={input}
+          className={styles.input}
+          type="search"
+          onKeyDown={handleKeyPress}
+        />
+        <button
+          className={styles.button}
+          onClick={() => {
+            props.onSearch(character);
+            setCharacter("");
+          }}
+        >
+          Agregar
+        </button>
+      </div>
+    </div>
+  );
 }
