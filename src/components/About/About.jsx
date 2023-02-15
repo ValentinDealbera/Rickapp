66bc4b1a8f61487a66cbd4cb9a040cb9fa22fa3b
ValentinDealbera <valentindealbera01@gmail.com>
2023-02-15 11:06:23 -0300

    About styles corrected

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,6 +13,7 @@ const About = () => {
                 pero nunca tuve el coraje para hacerlo, hasta hoy, y gracias a los conocimientos
                 que Soyhenry me esta dando, he sido capaz de construir esta aplicacion.
             </p>
+            <img className={styles.img} src="https://scontent.frcu1-1.fna.fbcdn.net/v/t39.30808-6/306008735_10221903951929812_6953663035864221868_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHOxLZOOZBzafefp4HYm7RD89kCxsOeaMLz2QLGw55owg2D3Grc3ckUik5GVdaBUg4&_nc_ohc=tdzfYfR572AAX__wram&_nc_ht=scontent.frcu1-1.fna&oh=00_AfAmJC3u8YOQyKnhjlXvJXSiNFXE8qnbG6CkWa0lQr3jJw&oe=63F1C2BF" alt="Valentin Dealbera" />
         </div>
     )
 }
