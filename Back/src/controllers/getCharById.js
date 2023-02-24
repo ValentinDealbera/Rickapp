7b1a44bb6097f9828a902ad7a1aafc9c37a04ab1
ValentinDealbera <111397786+ValentinDealbera@users.noreply.github.com>
2023-02-24 11:13:11 -0300

    manual error handler

--- Back/src/controllers/getCharById.js
+++ Back/src/controllers/getCharById.js
@@ -1,21 +1,27 @@
-var fetch = require('node-fetch')
+var fetch = require("node-fetch");
 const getCharById = (res, id) => {
-    fetch(`https://rickandmortyapi.com/api/character/${id}`)
+  fetch(`https://rickandmortyapi.com/api/character/${id}`)
     .then((response) => response.json())
     .then((data) => {
-            const aux = {
-                id: data.id,
-                image: data.image,
-                name: data.name,
-                gender: data.gender,
-                species: data.species
-                }
-            res.writeHead(200, {'Content-Type':'application/json'})
-            res.end(JSON.stringify(aux));
-    }).catch(err => {
-        res.writeHead(500, {'Content-Type':'text/plain'})
-        res.end(err);
+      if (data.name) {
+        const aux = {
+          id: data.id,
+          image: data.image,
+          name: data.name,
+          gender: data.gender,
+          species: data.species,
+        };
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(aux));
+      } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Character not Found");
+      }
     })
-}
+    .catch((err) => {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end(err);
+    });
+};
 
-module.exports= getCharById
+module.exports = getCharById;
