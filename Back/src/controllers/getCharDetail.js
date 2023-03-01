2abf3b49269ec02ee7bb80d59dcf56757d056077
Valentin Dealbera <valentindealbera01@gmail.com>
2023-03-01 16:14:11 -0300

    all set

--- Back/src/controllers/getCharDetail.js
+++ Back/src/controllers/getCharDetail.js
@@ -4,7 +4,6 @@ async function getCharDetail(req, res) {
   const { detailId } = req.params;
   try {
     const response = await axios(URL + detailId)
-    .then(response => {
       const character = {
         id: response.data.id,
         name: response.data.name,
@@ -14,7 +13,6 @@ async function getCharDetail(req, res) {
         origin: response.data.origin,
       }
       res.status(200).json(character)
-    })
   } catch (error) {
     res.status(404).send(error.message)
   }
